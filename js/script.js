b9c3252b263f070cd997c12f3fdcbd03da1e947a
Anna Piwnik <a.piwnik@o2.pl>
2018-01-21 19:29:04 +0100

    delete old code

--- js/script.js
+++ js/script.js
@@ -32,35 +32,3 @@ $(document).ready(function() {
         getQuote();
     })
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var url = 'http://api.icndb.com/jokes/random';
-
-var $button = $('#get-joke').click(function() {
-	getJoke();
-});
-
-var $paragraph = $('#joke');
-
-function getJoke() {
-  $.ajax({
-  	method: 'GET',
-  	url: url,
-  	success: function(res) {
-  		$paragraph.text(res.value.joke);
-  	}
-  });
-}
